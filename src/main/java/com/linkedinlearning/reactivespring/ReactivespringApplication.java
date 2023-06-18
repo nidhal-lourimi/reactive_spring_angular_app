de73a8c06113b4f08c672aa6802fa0e59e18f313
nidhal louremi <52574003+nidhal-lourimi@users.noreply.github.com>
2023-06-18 13:20:22 +0100

    implementing reactive spring data

--- src/main/java/com/linkedinlearning/reactivespring/ReactivespringApplication.java
+++ src/main/java/com/linkedinlearning/reactivespring/ReactivespringApplication.java
@@ -2,8 +2,10 @@ package com.linkedinlearning.reactivespring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class ReactivespringApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/linkedinlearning/reactivespring/controler/ReservationResource.java
+++ src/main/java/com/linkedinlearning/reactivespring/controler/ReservationResource.java
@@ -1,7 +1,8 @@
 package com.linkedinlearning.reactivespring.controler;
 
 import com.linkedinlearning.reactivespring.model.Reservation;
-import org.springframework.data.repository.query.Param;
+import com.linkedinlearning.reactivespring.service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
@@ -11,22 +12,29 @@ import reactor.core.publisher.Mono;
 @CrossOrigin
 public class ReservationResource {
     public static final String ROOM_V_1_RESERVATION = "/room/v1/reservation";
-    @GetMapping(path = "{roomId}",produces = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<String> getReservationById(@PathVariable String roomId){
-        return Mono.just("{}");
+    private final ReservationService reservationService;
+@Autowired
+    public ReservationResource(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @GetMapping(path = "{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<Reservation> getReservationById(@PathVariable String id){
+        return reservationService.getReservation(id);
     }
    @PostMapping(path = "",consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<String> createReservation(@RequestBody Mono<Reservation> reservation){
-       return Mono.just("{post}");
+    public Mono<Reservation> createReservation(@RequestBody Mono<Reservation> reservation){
+       return reservationService.createReservation(reservation);
 
     }
     @PutMapping(path = "{roomId}",produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
-    public  Mono<String> updatePrices(@PathVariable String roomId){
-        return Mono.just("{delete}");
+    public  Mono<Reservation> updatePrices(@PathVariable String roomId,@RequestBody Mono<Reservation> reservation){
+        return reservationService.updateReservation(roomId,reservation);
     }
-    @DeleteMapping(path = "{roomId}",produces = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<Boolean> deleteReservationById(@PathVariable String roomId){
-        return Mono.just(true);}
+    @DeleteMapping(path = "{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<Boolean> deleteReservationById(@PathVariable String id) {
+        return reservationService.deleteReservation(id);
+    }
 
 
 
